32fcc6c3e09728d4b485b37dfbfb090fded9e57e
elyamanihajar <hajar.elyamani03@gmail.com>
2025-10-31 22:29:17 +0100

    Part3

--- customer-service/src/main/java/ma/emsi/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/ma/emsi/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,16 @@
 package ma.emsi.customerservice;
 
+import ma.emsi.customerservice.config.CustomerConfigParams;
 import ma.emsi.customerservice.entities.Customer;
 import ma.emsi.customerservice.repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CustomerConfigParams.class)
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
