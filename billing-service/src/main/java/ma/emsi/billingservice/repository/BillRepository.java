365fa88cb6b01b17cb23174f0ff5cc1c50cf3634
elyamanihajar <hajar.elyamani03@gmail.com>
2025-11-13 18:20:39 +0100

    billRepo change

--- billing-service/src/main/java/ma/emsi/billingservice/repository/BillRepository.java
+++ billing-service/src/main/java/ma/emsi/billingservice/repository/BillRepository.java
@@ -2,6 +2,14 @@ package ma.emsi.billingservice.repository;
 
 import ma.emsi.billingservice.entities.Bill;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
+import java.util.List;
+@RepositoryRestResource
 public interface BillRepository extends JpaRepository<Bill, Long> {
+
+    @RestResource(path = "/byCustomerID")
+    List<Bill> findByCustomerId(@Param("customerId") long customerId);
 }
